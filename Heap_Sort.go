378d01daff3a268e7942856c146cced92ab44c3f
stebinsabu13 <60867706+stebinsabu13@users.noreply.github.com>
2023-03-23 09:59:10 +0530

    Add files via upload

---
+++ Heap_Sort.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func heapSort(arr []int) {
+	n := len(arr)
+	for i := (len(arr) / 2) - 1; i >= 0; i-- {
+		heapify(arr, n, i)
+	}
+	for i := n - 1; i >= 0; i-- {
+		arr[0], arr[i] = arr[i], arr[0]
+		heapify(arr, i, 0)
+	}
+}
+func heapify(arr []int, n, i int) {
+	largest := i
+	lc := lchild(i)
+	rc := rchild(i)
+	if lc < n && arr[largest] < arr[lc] {
+		largest = lc
+	}
+	if rc < n && arr[largest] < arr[rc] {
+		largest = rc
+	}
+	if largest != i {
+		arr[i], arr[largest] = arr[largest], arr[i]
+		heapify(arr, n, largest)
+	}
+}
+func lchild(i int) int {
+	return (i * 2) + 1
+}
+func rchild(i int) int {
+	return (i * 2) + 2
+}
+func main() {
+	arr := []int{10, 20, 15, 12, 40, 25, 18}
+	heapSort(arr)
+	fmt.Println(arr)
+}
